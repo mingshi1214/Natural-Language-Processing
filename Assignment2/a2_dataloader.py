fa3b89bf85d4a806d9831230e8f889129ab96674
Mingshi <38367310+mingshi1214@users.noreply.github.com>
2021-05-14 11:15:39 -0400

    Update a2_dataloader.py

--- Assignment2/a2_dataloader.py
+++ Assignment2/a2_dataloader.py
@@ -1,23 +1,3 @@
-'''
-This code is provided solely for the personal and private use of students
-taking the CSC401H/2511H course at the University of Toronto. Copying for
-purposes other than this use is expressly prohibited. All forms of
-distribution of this code, including but not limited to public repositories on
-GitHub, GitLab, Bitbucket, or any other online platform, whether as given or
-with any changes, are expressly prohibited.
-
-Authors: Sean Robertson, Jingcheng Niu, Zining Zhu, and Mohamed Abdall
-
-All of the files in this directory and all subdirectories are:
-Copyright (c) 2021 University of Toronto
-'''
-
-'''Build Datasets for Hansards
-
-Don't go publishing results based on this. We restrict a lot of things to make
-this nicer.
-'''
-
 import locale
 import os
 import re
